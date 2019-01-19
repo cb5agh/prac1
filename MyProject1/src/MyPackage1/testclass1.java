e82576e7cea48fa9d9b3739cf24a7c5cbbbf9642
cb5agh <ghoffar.alvi@googlemail.com>
2019-01-19 18:58:14 +0500

    My second eclips project

---
+++ MyProject1/src/MyPackage1/testclass1.java
@@ -0,0 +1,13 @@
+package MyPackage1;
+
+public class testclass1 {
+
+	public static void main(String[] args) {
+		System.out.println("This Java program written in Eclips... ");
+		System.out.print("testclass 1... ");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
